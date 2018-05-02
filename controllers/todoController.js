07b0287b3165cbbb8537ef413e0b8954064bb95c
nandodonaire <nandodonaire@gmail.com>
2018-05-02 16:47:45 -0400

    Added GET functionality to view the rows in sheet
    --Also updated the todo.ejs file to display just the task name for now

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,12 +2,43 @@ let bodyParser = require('body-parser')
 let urlencodedParser = bodyParser.urlencoded({extended: false})
 let mongoose = require('mongoose')
 
+const config = require('../config.json')
+
+// requiring Smartsheet
+let client = require('smartsheet')
+let smartsheet = client.createClient({ accessToken: config.SMARTSHEET_ACCESS_TOKEN})
+let sheetId = config.SHEET_ID
+
 let data = [{item: 'Get Milk'}, {item: 'Work on Smartsheet app'}, {item: 'Walk Basil'}]
+
+const ToDo = function (id, task, status, dueDate) {
+  this.id = id
+  this.toDoText = task
+  this.status = status
+  this.dueDate = dueDate
+}
 
 module.exports = function (app) {
 
+// Route to get to do tasks from Smartsheet
   app.get('/todo', function (req, res) {
-    res.render('todo', {todos: data})
+    // res.render('todo', {todos: data})
+    let options = {
+      id: sheetId
+    }
+    smartsheet.sheets.getSheet(options)
+    .then(function(sheetInfo) {
+      let sheetRows = []
+      sheetInfo.rows.forEach(function (row) {
+        let task = new ToDo (row.id, row.cells[0].value, row.cells[1].value, row.cells[2].value)
+        sheetRows.push(task)
+      })
+      console.log(sheetRows);
+      res.render('todo', {todos: sheetRows})
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
   })
 
   app.post('/todo', urlencodedParser, function (req, res) {
